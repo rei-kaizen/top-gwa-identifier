5ddc5a5896e344bc4067bdf548c73931e385a184
rei-kaizen <111290781+rei-kaizen@users.noreply.github.com>
2023-04-25 15:16:32 +0800

    update output to include list of names with each gwa

--- random-names-gwa-generator.py
+++ random-names-gwa-generator.py
@@ -9,4 +9,19 @@ def gwa_generator():
     #return the gwa to 2 decimal places
     return round(random_gwa, 2)
 
-print(gwa_generator())
+#have a list of student names
+names = ["Choi Seungcheol", "Yoon Jeonghan", "Hong Jisoo", "Wen Junhui", "Kwon Soonyoung",
+         "Lee Wonwoo", "Lee Jihoon", "Lee Seokmin", "Kim Mingyu", "Xu Minghao",
+         "Boo Seungkwan", "Choi Hansol", "Lee Chan", "Tumas Kong", "Ading Khel",
+         "Canny Vincent", "Oniz Nano", "Harlong Short", "Qiqi Batas", "Lucky White"]
+
+#initialize an empty list to store the student's record
+marklist = []
+#loop through the names of the students
+for name in names:
+    #call the gwa generator function to get the random gwa
+    gwa = gwa_generator()
+    #append the dictionary contains the student's name and gwa as keys to the marklist
+    marklist.append({"name": name, "gwa" : gwa})
+    
+    print(f"{name}: {gwa}\n")
