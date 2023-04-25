b6c603b4ce7ad12ff298ff71fc7f2a73f725d5c9
rei-kaizen <111290781+rei-kaizen@users.noreply.github.com>
2023-04-26 00:26:15 +0800

    convert contents to a list of dict

--- highest_gwa.py
+++ highest_gwa.py
@@ -2,8 +2,19 @@
 file = open("students.txt", "r")
 contents = file.readlines()
 
-print(contents)
-
 #convert all the contents to a list of dictionaries
+#initialize an empty list for marklist1
+marklist1 = []
+#loop through the contents
+for line in contents:
+    #split the name and gwa to each other
+    name, gwa = line.split(":")
+    #and remove the whitespaces characters and add it to the list
+    marklist1.append({"name" : name.strip(), "gwa": float(gwa.strip())}) 
+
+print(marklist1)
+
 #find the top student with the highest gwa
 #print the output
+
+file.close()
